219bb3656f37e2ec31daeb4ee129e3bee6fe6601
thangnd05 <thangnd.contact@gmail.com>
2026-03-08 21:13:28 +0700

    refactor(pratice): replace entity with DTO in controller

--- src/main/java/com/example/english_exam/controllers/PracticeController.java
+++ src/main/java/com/example/english_exam/controllers/PracticeController.java
@@ -1,10 +1,11 @@
 package com.example.english_exam.controllers;
 
 import com.example.english_exam.dto.request.PracticeCheckRequest;
+import com.example.english_exam.dto.response.PracticeCheckResponse;
+import com.example.english_exam.dto.response.PracticeQuestionResponse;
 import com.example.english_exam.services.LearningVoca.PracticeService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,28 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class PracticeController {
 
-    @Autowired
-    private PracticeService practiceService;
-
+    private final PracticeService practiceService;
 
     @GetMapping("/generate/{albumId}")
-    public ResponseEntity<?> generate(HttpServletRequest request, @PathVariable Long albumId) {
+    public ResponseEntity<PracticeQuestionResponse> generate(HttpServletRequest request, @PathVariable Long albumId) {
         return practiceService.generateOneRandomQuestion(request, albumId)
-                .<ResponseEntity<?>>map(ResponseEntity::ok)
+                .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.noContent().build());
     }
+
     @PostMapping("/mark-known/{vocabId}")
-    public ResponseEntity<?> markWordKnown(@PathVariable Long vocabId, HttpServletRequest httpRequest) {
+    public ResponseEntity<String> markWordKnown(@PathVariable Long vocabId, HttpServletRequest httpRequest) {
         practiceService.markWordAsKnown(httpRequest, vocabId);
         return ResponseEntity.ok("Đã đánh dấu từ này là đã biết");
     }
 
     @PostMapping("/check")
-    public ResponseEntity<?> check(HttpServletRequest request, @RequestBody PracticeCheckRequest req) {
+    public ResponseEntity<PracticeCheckResponse> check(HttpServletRequest request, @RequestBody PracticeCheckRequest req) {
         return ResponseEntity.ok(practiceService.checkAnswer(request, req));
     }
-
-
-
 }
 
